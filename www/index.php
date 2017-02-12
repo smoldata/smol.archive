dc6e25f4679361930c7148142a72d6fc3a8024ce
Dan Phiffer <dan@phiffer.org>
2017-02-12 16:50:56 -0500

    :wrench: just show the basics

--- www/index.php
+++ www/index.php
@@ -6,12 +6,6 @@
 	
 	if ($GLOBALS['cfg']['user']){
 		$twitter_accounts = twitter_users_get_accounts($GLOBALS['cfg']['user']);
-		foreach ($twitter_accounts as $index => $account){
-			$info = twitter_api_get($account, 'users/show', array(
-				'user_id' => $account['twitter_id']
-			));
-			$twitter_accounts[$index] = array_merge($info, $account);
-		}
 		$GLOBALS['smarty']->assign_by_ref('twitter_accounts', $twitter_accounts);
 		$GLOBALS['smarty']->display('page_home.txt');
 	} else {
