d5fcf7598593d1d8b509de6662fadd2ce40ba272
Dan Phiffer <dan@phiffer.org>
2017-03-17 21:16:52 +0000

    :wrench: use sudo when setting up unattended-upgrades

--- debian/setup-debian.sh
+++ debian/setup-debian.sh
@@ -3,4 +3,4 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install -y make git emacs24-nox htop sysstat ufw fail2ban unattended-upgrades unzip imagemagick
-echo "unattended-upgrades       unattended-upgrades/enable_auto_updates boolean true" | debconf-set-selections; dpkg-reconfigure -f noninteractive unattended-upgrades
+sudo echo "unattended-upgrades       unattended-upgrades/enable_auto_updates boolean true" | sudo debconf-set-selections; sudo dpkg-reconfigure -f noninteractive unattended-upgrades
