fa5b12fe7eeeddc3daeab2ebc2b39f1472640baf
Dan Phiffer <dan@phiffer.org>
2017-03-27 20:46:00 -0400

    :wrench: trying something for the Twitter API

--- www/include/lib_twitter_api.php
+++ www/include/lib_twitter_api.php
@@ -60,7 +60,8 @@
 		$method = 'POST';
 		$query = http_build_query($params);
 		$url = $GLOBALS['cfg']['twitter_api_endpoint'] . $path . '.json';
-		
+		$url .= "?$query";
+
 		$token = array(
 			'key' => $account['access_token'],
 			'secret' => $account['access_secret']
@@ -71,7 +72,7 @@
 		);
 		$more = twitter_api_http_more();
 		
-		$rsp = http_post($url, $query, $headers, $more);
+		$rsp = http_post($url, '', $headers, $more);
 		if (! $rsp['ok']){
 			return $rsp;
 		}
